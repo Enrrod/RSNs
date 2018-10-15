ae0617e9394b27474b0cf858c1845cb59a701e98
Enrrod <enrique.rodsebastian@gmail.com>
2018-10-15 13:53:00 +0200

    Analysis made with adjacencies instead of weighted matrices

--- Programs/graphAnalysis.py
+++ Programs/graphAnalysis.py
@@ -10,21 +10,23 @@ def readgraph(x):
     return g
 
 
-def wmatrix(gph):
+def adjmatrix(gph):
     A = nx.adjacency_matrix(gph)
-    W = A.toarray()
-    W = W.astype(float)
-    peso_max = np.amax(W)
-    W = W / peso_max
-    return W
+    A = A.toarray()
+    A = A.astype(float)
+    peso_max = np.amax(A)
+    A = A / peso_max
+    #A[np.where(A < thres)] = 0
+    A[np.where(A > 0)] = 1
+    return A
 
-def degmatrix(W):
-    tam = W.shape[0]
+def degmatrix(A):
+    tam = A.shape[0]
     d = np.zeros((tam, 1))
     I = np.identity(tam)
     for i in range(tam):
         for j in range(tam):
-            d[i] = d[i] + W[i, j]
+            d[i] = d[i] + A[i, j]
     D = I * d
     return D
 
@@ -60,19 +62,20 @@ if __name__=="__main__":
         print "Processing subject " + name + "\n"
         graph = directory + '/' + subjects[i]
         G = readgraph(graph)
-        W = wmatrix(G)
-        D = degmatrix(W)
-        gL = graphLaplacian(W, D)
+        A = adjmatrix(G)
+        D = degmatrix(A)
+        gL = graphLaplacian(A, D)
         print "Diagonalizing... \n"
         PHY, LANDA = eigen(gL)
+        PHY, LANDA = PHY.real, LANDA.real
         print "Projecting eigenvectors... \n"
-        eigenProjection = np.empty((0,len(W)))
+        eigenProjection = np.empty((0,len(A)))
         for j in range(len(PHY)):
-            eigenProjection = np.vstack((eigenProjection, np.dot(PHY[j], W)))
+            eigenProjection = np.vstack((eigenProjection, np.dot(PHY[j], A)))
             print "Projected eigenvector (" + str(j + 1) + "/"+ str(len(PHY)) + ")"
-        wName = '/home/enrique/Proyectos/RSNs/Data/DS01216/Wmatrix/Wmat_' + name + '.csv'
-        projName = '/home/enrique/Proyectos/RSNs/Data/DS01216/EigenProjection/eigenProjection_' + name + '.csv'
-        np.savetxt(wName, W, delimiter=',')
+        wName = '/home/enrique/Proyectos/RSNs/Data/DS01216/umbralized/Amat/Amat_' + name + '.csv'
+        projName = '/home/enrique/Proyectos/RSNs/Data/DS01216/umbralized/EigenProjection/eigenProjection_' + name + '.csv'
+        np.savetxt(wName, A, delimiter=',')
         np.savetxt(projName, eigenProjection, delimiter=',')
         print "Data saved"
 
